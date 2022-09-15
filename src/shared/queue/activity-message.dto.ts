f8b33d955490ee3061cc34c160129d71365b0cc2
Mark Tripoli <mgtripoli@comcast.net>
2022-09-15 19:40:34 -0400

    add in push tokens to limit queries in consumer

--- src/shared/queue/activity-message.dto.ts
+++ src/shared/queue/activity-message.dto.ts
@@ -1,6 +1,8 @@
 import {ActivityType} from "./activity-type";
+import {DevicePushToken} from "@prisma/client";
 
 export interface ActivityMessageDto<T> {
     activityType: ActivityType;
     message: T;
+    devicePushTokens?: DevicePushToken[];
 }

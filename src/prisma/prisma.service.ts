b0a4a58658a42972dc66fa3ea4359b2516fd977e
Mark Tripoli <mgtripoli@comcast.net>
2022-09-20 00:43:27 -0400

    remove debug logs

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,30 +1,8 @@
-import {INestApplication, Injectable, OnModuleInit} from '@nestjs/common';
+import { INestApplication, Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    super({
-      log: [
-        {
-          emit: 'event',
-          level: 'query',
-        },
-      ],
-    });
-  }
-
-  async onModuleInit(): Promise<void> {
-    await this.$connect();
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    this.$on('query', async (e) => {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      console.log(`${e.query} ${e.params}`);
-    });
-  }
-
+export class PrismaService extends PrismaClient {
   async enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
       await app.close();

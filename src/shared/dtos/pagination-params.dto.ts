6888179e874ac183c90f01a2b5a5350e4f19defe
Mark Tripoli <mgtripoli@comcast.net>
2022-10-05 17:32:54 -0400

    Change from offset to cursor based pagination for Ghillies

--- src/shared/dtos/pagination-params.dto.ts
+++ src/shared/dtos/pagination-params.dto.ts
@@ -3,23 +3,23 @@ import { Transform } from 'class-transformer';
 import { IsNumber, IsOptional, Min } from 'class-validator';
 
 export class PaginationParamsDto {
-  @ApiPropertyOptional({
-    description: 'Optional, defaults to 100',
-    type: Number,
-  })
-  @IsNumber()
-  @IsOptional()
-  @Min(0)
-  @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
-  limit = 100;
+    @ApiPropertyOptional({
+        description: 'Optional, defaults to 100',
+        type: Number,
+    })
+    @IsNumber()
+    @IsOptional()
+    @Min(0)
+    @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
+    limit = 25;
 
-  @ApiPropertyOptional({
-    description: 'Optional, defaults to 0',
-    type: Number,
-  })
-  @IsNumber()
-  @IsOptional()
-  @Min(0)
-  @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
-  offset = 0;
+    @ApiPropertyOptional({
+        description: 'Optional, defaults to 0',
+        type: Number,
+    })
+    @IsNumber()
+    @IsOptional()
+    @Min(0)
+    @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
+    offset = 0;
 }
--- src/shared/ghillies/ghillie-search.criteria.ts
+++ src/shared/ghillies/ghillie-search.criteria.ts
@@ -1,30 +1,38 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 import { GhillieStatus } from '@prisma/client';
-import { PaginationParamsDto } from '../dtos';
+import { Type } from 'class-transformer';
 
-export class GhillieSearchCriteria extends PaginationParamsDto {
-  @ApiProperty()
-  @IsOptional()
-  name?: string;
+export class GhillieSearchCriteria {
+    @IsNumber()
+    @Type(() => Number)
+    take = 25;
 
-  @ApiProperty()
-  @IsOptional()
-  slug?: string;
+    @IsString()
+    @IsOptional()
+    cursor?: string;
 
-  @ApiProperty()
-  @IsOptional()
-  about?: string;
+    @ApiProperty()
+    @IsOptional()
+    name?: string;
 
-  @ApiProperty()
-  @IsOptional()
-  status?: GhillieStatus = GhillieStatus.ACTIVE;
+    @ApiProperty()
+    @IsOptional()
+    slug?: string;
 
-  @ApiProperty()
-  @IsOptional()
-  readonly?: boolean;
+    @ApiProperty()
+    @IsOptional()
+    about?: string;
 
-  @ApiProperty()
-  @IsOptional()
-  topicIds?: Array<string>;
+    @ApiProperty()
+    @IsOptional()
+    status?: GhillieStatus = GhillieStatus.ACTIVE;
+
+    @ApiProperty()
+    @IsOptional()
+    readonly?: boolean;
+
+    @ApiProperty()
+    @IsOptional()
+    topicIds?: Array<string>;
 }

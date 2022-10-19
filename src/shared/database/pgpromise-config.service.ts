1b9081bdbdbf6dd7303e1f7d497d7fdac5f1014a
Mark Tripoli <mgtripoli@comcast.net>
2022-10-19 00:21:22 -0400

    pg-convert to camel case

--- src/shared/database/pgpromise-config.service.ts
+++ src/shared/database/pgpromise-config.service.ts
@@ -1,20 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as pgPromise from 'pg-promise';
 import { NestPgpromiseOptionsFactory } from 'nestjs-pgpromise/dist/interfaces/nest-pgpromise-options-factory.interface';
 import { NestPgpromiseOptions } from 'nestjs-pgpromise/dist/interfaces/nest-pgpromise-options.interface';
 
 @Injectable()
 export class PgPromiseConfigService implements NestPgpromiseOptionsFactory {
-  constructor(private configService: ConfigService) {}
+    constructor(private configService: ConfigService) {}
 
-  createNestPgpromiseOptions(): NestPgpromiseOptions {
-    const DATABASE_URL = this.configService.get('databaseUrl');
-    return {
-      connection: {
-        connectionString: DATABASE_URL,
-        connectionTimeoutMillis: 5000,
-        idleTimeoutMillis: 10000,
-      },
-    };
-  }
+    createNestPgpromiseOptions(): NestPgpromiseOptions {
+        const DATABASE_URL = this.configService.get('databaseUrl');
+        return {
+            connection: {
+                connectionString: DATABASE_URL,
+                connectionTimeoutMillis: 5000,
+                idleTimeoutMillis: 10000,
+            },
+            initOptions: {
+                receive: (data, result, e) => {
+                    this.camelizeColumns(data);
+                },
+            },
+        };
+    }
+
+    private camelizeColumns(data) {
+        const template = data[0];
+        for (const prop in template) {
+            const camel = pgPromise.utils.camelize(prop);
+            if (!(camel in template)) {
+                for (let i = 0; i < data.length; i++) {
+                    const d = data[i];
+                    d[camel] = d[prop];
+                    delete d[prop];
+                }
+            }
+        }
+    }
 }
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -10,42 +10,42 @@ import { PgPromiseConfigService } from './database/pgpromise-config.service';
 import { GetStreamModule } from './getsream/getstream.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(configModuleOptions),
-    AppLoggerModule,
-    MailModule,
-    MailerModule.forRootAsync({
-      useClass: MailConfigService,
-    }),
-    NestPgpromiseModule.registerAsync({
-      useClass: PgPromiseConfigService,
-    }),
-    SnsModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        region: configService.get<string>('aws.region'),
-      }),
-      inject: [ConfigService],
-      isGlobal: true,
-    }),
-    GetStreamModule.registerAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => {
-        return {
-          apiKey: configService.get('stream.apiKey'),
-          apiSecret: configService.get('stream.apiSecret'),
-          appId: configService.get('stream.appId'),
-          clientOptions: {
-            timeout: 10000,
-          },
-        };
-      },
-      inject: [ConfigService],
-      isGlobal: true,
-    }),
-  ],
-  controllers: [],
-  providers: [],
-  exports: [AppLoggerModule, ConfigModule, MailModule],
+    imports: [
+        ConfigModule.forRoot(configModuleOptions),
+        AppLoggerModule,
+        MailModule,
+        MailerModule.forRootAsync({
+            useClass: MailConfigService,
+        }),
+        NestPgpromiseModule.registerAsync({
+            useClass: PgPromiseConfigService,
+        }),
+        SnsModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                region: configService.get<string>('aws.region'),
+            }),
+            inject: [ConfigService],
+            isGlobal: true,
+        }),
+        GetStreamModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => {
+                return {
+                    apiKey: configService.get('stream.apiKey'),
+                    apiSecret: configService.get('stream.apiSecret'),
+                    appId: configService.get('stream.appId'),
+                    clientOptions: {
+                        timeout: 10000,
+                    },
+                };
+            },
+            inject: [ConfigService],
+            isGlobal: true,
+        }),
+    ],
+    controllers: [],
+    providers: [],
+    exports: [AppLoggerModule, ConfigModule, MailModule],
 })
 export class SharedModule {}

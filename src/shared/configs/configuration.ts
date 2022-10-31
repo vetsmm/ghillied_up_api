b025fd22ad7466c038c66231f947d016cb8d951e
Mark Tripoli <mgtripoli@comcast.net>
2022-10-31 10:52:41 -0400

    remove unused env vars

--- src/shared/configs/configuration.ts
+++ src/shared/configs/configuration.ts
@@ -25,7 +25,6 @@ export default (): any => ({
         },
     },
     sentryDsn:
-        process.env.SENTRY_DSN ??
         'https://324fb0002f5b4cde8686245e89346503@o228030.ingest.sentry.io/6615762',
     auth: {
         passwordResetTokenExpiryInMs:
@@ -60,7 +59,7 @@ export default (): any => ({
         user: process.env.MAIL_USER,
         password: process.env.MAIL_PASSWORD,
         defaultEmail: process.env.MAIL_DEFAULT_EMAIL,
-        defaultName: process.env.MAIL_DEFAULT_NAME,
+        defaultName: 'Ghillied Up',
         ignoreTLS: process.env.MAIL_IGNORE_TLS === 'true',
         secure: process.env.MAIL_SECURE === 'true',
         requireTLS: process.env.MAIL_REQUIRE_TLS === 'true',

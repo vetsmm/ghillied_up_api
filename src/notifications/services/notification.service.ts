dd2d322fb4f88e5077ec644f71bfd9bab16bc61f
Mark Tripoli <mgtripoli@comcast.net>
2022-12-10 22:23:26 -0500

    fix ordering of hydrated notification objects

--- src/notifications/services/notification.service.ts
+++ src/notifications/services/notification.service.ts
@@ -102,7 +102,7 @@ export class NotificationService {
             });
         });
 
-        return this.pg
+        const response: any[] = await this.pg
             .task(async (t) => {
                 const reactionIds =
                     notificationsByType.get(NotificationType.POST_REACTION) ||
@@ -118,23 +118,23 @@ export class NotificationService {
                 const postCommentsHydrated =
                     commentIds.length > 0
                         ? t.query(
-                              'SELECT pc.id as "sourceId", pc."created_date", pc.content as "commentContent", u.username, g.id as "ghillieId", g.name as "ghillie_name", g."image_url" as "ghillieImageUrl",p.id as "postId" FROM "post_comment" pc JOIN "user" u ON pc."created_by_id" = u.id JOIN "post" p ON pc."post_id" = p.id JOIN "ghillie" g on g.id = p."ghillie_id"WHERE pc.id IN ($1:list)',
-                              [commentIds],
-                          )
+                            'SELECT pc.id as "sourceId", pc."created_date", pc.content as "commentContent", u.username, g.id as "ghillieId", g.name as "ghillie_name", g."image_url" as "ghillieImageUrl",p.id as "postId" FROM "post_comment" pc JOIN "user" u ON pc."created_by_id" = u.id JOIN "post" p ON pc."post_id" = p.id JOIN "ghillie" g on g.id = p."ghillie_id"WHERE pc.id IN ($1:list)',
+                            [commentIds],
+                        )
                         : [];
                 const postCommentReactionsHydrated =
                     commentReactionIds.length > 0
                         ? t.query(
-                              'SELECT cr.id as "sourceId", cr."created_date", cr."reaction_type", u.username, g.id as "ghillieId", g.name as "ghillieName", g."image_url" as "ghillieImageUrl", p.id as "postId" FROM "comment_reaction" cr JOIN "user" u ON cr."created_by_id" = u.id JOIN "post_comment" pc ON cr."comment_id" = pc.id JOIN "post" p ON pc."post_id" = p.id JOIN "ghillie" g on g.id = p."ghillie_id" WHERE cr.id IN ($1:list)',
-                              [commentReactionIds],
-                          )
+                            'SELECT cr.id as "sourceId", cr."created_date", cr."reaction_type", u.username, g.id as "ghillieId", g.name as "ghillieName", g."image_url" as "ghillieImageUrl", p.id as "postId" FROM "comment_reaction" cr JOIN "user" u ON cr."created_by_id" = u.id JOIN "post_comment" pc ON cr."comment_id" = pc.id JOIN "post" p ON pc."post_id" = p.id JOIN "ghillie" g on g.id = p."ghillie_id" WHERE cr.id IN ($1:list)',
+                            [commentReactionIds],
+                        )
                         : [];
                 const postReactionsHydrated =
                     reactionIds.length > 0
                         ? t.query(
-                              'SELECT pr.id as "sourceId", pr."created_date", pr."reaction_type", u.username, g.id as "ghillieId", g.name as "ghillieName", g."image_url" as "ghillieImageUrl", p.id as "postId" FROM "post_reaction" pr JOIN "user" u ON pr."created_by_id" = u.id JOIN "post" p ON pr."post_id" = p.id JOIN "ghillie" g on g.id = p."ghillie_id" WHERE pr.id IN ($1:list)',
-                              [reactionIds],
-                          )
+                            'SELECT pr.id as "sourceId", pr."created_date", pr."reaction_type", u.username, g.id as "ghillieId", g.name as "ghillieName", g."image_url" as "ghillieImageUrl", p.id as "postId" FROM "post_reaction" pr JOIN "user" u ON pr."created_by_id" = u.id JOIN "post" p ON pr."post_id" = p.id JOIN "ghillie" g on g.id = p."ghillie_id" WHERE pr.id IN ($1:list)',
+                            [reactionIds],
+                        )
                         : [];
 
                 return [
@@ -188,6 +188,14 @@ export class NotificationService {
                     'Error retrieving notifications',
                 );
             });
+
+        // sort the response by created date descending
+        return response.sort((a, b) => {
+            return (
+                new Date(b.createdDate).getTime() -
+                new Date(a.createdDate).getTime()
+            );
+        });
     }
 
     async markAllNotificationsAsRead(ctx: RequestContext): Promise<void> {
